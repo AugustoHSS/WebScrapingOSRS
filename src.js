5932aaa6e72ef312f17a680c99b5c219e65ec18d
augusto hansel schaefer dos santos <augustohansel@hotmail.com>
2024-05-27 20:37:08 -0300

    webscrafing for all itens url for osrs wiki

---
+++ src.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const baseUrl = 'https://oldschool.runescape.wiki';
+const itemCategoryUrl = `${baseUrl}/w/Category:Equipable_items`;
+
+const allItemLinks = [];
+
+async function fetchAllItems(currentPageUrl = itemCategoryUrl) {
+  try {
+    const response = await axios.get(currentPageUrl);
+    const $ = cheerio.load(response.data);
+
+    const itemLinks = [];
+    $("div.mw-category").find('a').each((index, element) => {
+      const linkUrl = $(element).attr('href');
+      itemLinks.push(linkUrl);
+    });
+
+    allItemLinks.push(...itemLinks);
+
+    const nextPageLink = $('a:contains("next page")').attr('href');
+    if (nextPageLink) {
+      await fetchAllItems(`${baseUrl}${nextPageLink}`);
+    }
+  } catch (error) {
+    console.error('Error fetching items:', error);
+  }
+}
+
+fetchAllItems().then(() => {
+  const fileContent = allItemLinks.join('\n');
+  fs.writeFileSync('item_links.txt', fileContent, 'utf8');
+  console.log('Item links saved to item_links.txt');
+});
